969e3db77a0339b68e980295a2e769d8184d9c4a
MarceloMax50 <49374397+MarceloMax50@users.noreply.github.com>
2020-02-19 21:29:25 -0300

    Tabuada

---
+++ AtividadeLPII_02/src/atividadelpii_02/AtividadeLPII_02.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividadelpii_02;
+
+/**
+ *
+ * @author Usuário
+ */
+public class AtividadeLPII_02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        int tabuada = 7;
+for(int x = 0; x <= 10; x++){
+   System.out.print("Tabuada do: " + tabuada);
+   System.out.print(x + " x " + tabuada + " = ");
+   System.out.println(x * tabuada);
+}
+    }
+    
+}
